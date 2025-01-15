ff27d40ff93a9fcf633636924d879d091833ad91
T0nia <edtoniaelohor@gmail.com>
2025-01-15 22:07:39 +0100

    added all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Banner2 from "./components/Banner/Banner2";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <main className='overflow-x-hidden bg-white text-dark'>
-    <Navbar />
+    <main className="overflow-x-hidden bg-white text-dark">
+      <Hero />
+      <Services />
+      <Banner />
+      <Subscribe />
+      <Banner2 />
+      <Footer />
     </main>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import {IoMdMenu} from "react-icons/io";
+import React from "react";
+import { IoMdMenu } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const NavbarMenu = [
   {
@@ -8,51 +9,60 @@ const NavbarMenu = [
     path: "/",
   },
   {
-    id: 1,
+    id: 2,
     title: "Services",
-    path: "#",
+    link: "#",
   },
-   {
-    id: 1,
+  {
+    id: 3,
     title: "About Us",
-    path: "#",
+    link: "#",
   },
   {
     id: 4,
     title: "Our Team",
-    path: "#",
+    link: "#",
   },
   {
     id: 5,
     title: "Contact Us",
-    path: "#",
+    link: "#",
   },
-]
-
+];
 const Navbar = () => {
   return (
-    <nav>
-    <div className="container py-10 flex justify-between items-center">
-      {/* logo */}
-      <div>
-        <h1 className='font-bold text-2xl'>The Learning Curve</h1>
-      </div>
-      {/* menu */}
-      <div className='hidden lg:block'>
-<ul className='flex items-center gap-3'>
-  {
-    NavbarMenu.map( (menu) => (
-<li key={menu.id}>
-<a href={menu.path}>{menu.title}</a>
-</li>
-    )
-    )
-  }
-</ul>
-      </div>
-      {/* hamburger */}
-      
-      </div>  
+    <nav className="relative z-20">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="container py-10 flex justify-between items-center"
+      >
+        {/* Logo section */}
+        <div>
+          <h1 className="font-bold text-2xl">The Coding Journey</h1>
+        </div>
+        {/* Menu section */}
+        <div className="hidden lg:block">
+          <ul className="flex items-center gap-3">
+            {NavbarMenu.map((menu) => (
+              <li key={menu.id}>
+                <a
+                  href={menu.path}
+                  className="inline-block py-2 px-3 hover:text-secondary relative group"
+                >
+                  <div className="w-2 h-2 bg-secondary absolute mt-4 rounded-full left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden"></div>
+                  {menu.title}
+                </a>
+              </li>
+            ))}
+            <button className="primary-btn">Sign In</button>
+          </ul>
+        </div>
+        {/* Mobile Hamburger menu section */}
+        <div className="lg:hidden">
+          <IoMdMenu className="text-4xl" />
+        </div>
+      </motion.div>
     </nav>
   );
 };
